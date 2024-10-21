fa36773fd288518f8852b5eb1aef06758f70c3b6
nickfu <net456@163.com>
2024-10-21 20:00:05 +0800

    v1.1

--- content.js
+++ content.js
@@ -1,26 +1,83 @@
-chrome.storage.local.remove(["gmgn_check_result"], function () {
-  var error = chrome.runtime.lastError;
-  if (error) {
-    console.error("remove error", error);
-  }
-});
+let CHROME_CACHE_ID = null;
+let CHROME_CACHE_NAME = null;
+
+const getCacheName = function () {
+  CHROME_CACHE_ID = window.location.pathname.split("/").pop();
+  let cache_name = `gmgn_check_${CHROME_CACHE_ID}`;
+  return cache_name;
+};
+
+const removeCache = function () {
+  CHROME_CACHE_NAME = getCacheName();
+  chrome.storage.local.remove([CHROME_CACHE_NAME], function () {
+    var error = chrome.runtime.lastError;
+    if (error) {
+      console.error("remove error", error);
+    }
+  });
+};
+
+//清空旧的缓存
+removeCache();
 
 const CACHE_DATA = {
   total_supply: 0,
   usd_price: 0,
 };
+
 const GMGN_CHECK_RESULT = {
-  renounced_mint: false,
-  top_10_holder: false,
-  not_black_list: false,
-  is_burn: false,
-  holder_count: false,
-  initial_quote_reserve: false,
-  quote_reserve: false,
-  market_value: false,
-  profit_check: false,
-  bad_wallet_check: false,
-  flow_out_check: false,
+  renounced_mint: {
+    value: null,
+    res: false,
+  },
+  top_10_holder: {
+    value: null,
+    res: false,
+  },
+  not_black_list: {
+    value: null,
+    res: false,
+  },
+  is_burn: {
+    value: null,
+    res: false,
+  },
+  holder_count: {
+    value: null,
+    res: false,
+  },
+  initial_quote_reserve: {
+    value: null,
+    res: false,
+  },
+  quote_reserve: {
+    value: null,
+    res: false,
+  },
+  market_value: {
+    value: null,
+    res: false,
+  },
+  profit_check: {
+    value: null,
+    res: false,
+  },
+  bad_wallet_check: {
+    value: null,
+    res: false,
+  },
+  flow_out_check: {
+    value: null,
+    res: false,
+  },
+};
+const updateStorage = async function () {
+  CHROME_CACHE_NAME = getCacheName();
+  console.log("uuuuuu", CHROME_CACHE_NAME);
+  await chrome.storage.local.set({
+    [CHROME_CACHE_NAME]: GMGN_CHECK_RESULT,
+  });
+  return true;
 };
 const checkMarketValue = async function () {
   console.log(
@@ -29,70 +86,64 @@ const checkMarketValue = async function () {
     CACHE_DATA.total_supply,
     CACHE_DATA.usd_price * CACHE_DATA.total_supply
   );
-  if (CACHE_DATA.usd_price * CACHE_DATA.total_supply > 80000) {
-    GMGN_CHECK_RESULT.market_value = true;
+  const value = parseInt(CACHE_DATA.usd_price * CACHE_DATA.total_supply);
+  GMGN_CHECK_RESULT.market_value.value = parseInt(value / 1000) + "k";
+  if (value > 80000) {
+    GMGN_CHECK_RESULT.market_value.res = true;
   } else {
-    GMGN_CHECK_RESULT.market_value = false;
+    GMGN_CHECK_RESULT.market_value.res = false;
   }
   //   return res;
-  await chrome.storage.local.set({
-    gmgn_check_result: GMGN_CHECK_RESULT,
-  });
+  await updateStorage();
 };
 const securityCheck = async function (data) {
-  //   const res = {
-  //     renounced_mint: false,
-  //     top_10_holder: false,
-  //     not_black_list: false,
-  //     is_burn: false,
-  //   };
   //放弃mint权限
   if (data?.renounced_mint == true) {
-    GMGN_CHECK_RESULT.renounced_mint = true;
+    GMGN_CHECK_RESULT.renounced_mint.res = true;
   }
   //前10持仓小于30%
   if (data?.top_10_holder_rate && data?.top_10_holder_rate < 0.3) {
-    GMGN_CHECK_RESULT.top_10_holder = true;
+    GMGN_CHECK_RESULT.top_10_holder.value =
+      parseInt(data.top_10_holder_rate * 100) + "%";
+    GMGN_CHECK_RESULT.top_10_holder.res = true;
+  } else {
+    GMGN_CHECK_RESULT.top_10_holder.res = false;
   }
   //是否黑名单
-  GMGN_CHECK_RESULT.not_black_list = !data.is_show_alert;
+  GMGN_CHECK_RESULT.not_black_list.res = data.renounced_freeze_account;
   //是否烧池子
-  burn_ratio = parseInt(data.burn_ratio);
-  GMGN_CHECK_RESULT.is_burn = burn_ratio >= 1 ? true : false;
-  //   return res;
-  await chrome.storage.local.set({
-    gmgn_check_result: GMGN_CHECK_RESULT,
-  });
+  //   burn_ratio = parseInt(data.burn_ratio);
+  GMGN_CHECK_RESULT.is_burn.res = data.burn_status == "burn" ? true : false;
+  await updateStorage();
 };
 const tokenInfoCheck = async function (data) {
-  //   const res = {
-  //     holder_count: false,
-  //   };
+  GMGN_CHECK_RESULT.holder_count.value = data.holder_count;
   if (data.holder_count > 300) {
-    GMGN_CHECK_RESULT.holder_count = true;
+    GMGN_CHECK_RESULT.holder_count.res = true;
+  } else {
+    GMGN_CHECK_RESULT.holder_count.res = true;
   }
   CACHE_DATA.total_supply = data.total_supply;
   checkMarketValue();
   //   return res;
-  await chrome.storage.local.set({
-    gmgn_check_result: GMGN_CHECK_RESULT,
-  });
+  await updateStorage();
 };
 const tokenPoolInfoCheck = async function (data) {
-  //   const res = {
-  //     initial_quote_reserve: false,
-  //     quote_reserve: false,
-  //   };
+  GMGN_CHECK_RESULT.initial_quote_reserve.value = parseInt(
+    data.initial_quote_reserve
+  );
   if (data.initial_quote_reserve > 79) {
-    GMGN_CHECK_RESULT.initial_quote_reserve = true;
+    GMGN_CHECK_RESULT.initial_quote_reserve.res = true;
+  } else {
+    GMGN_CHECK_RESULT.initial_quote_reserve.res = false;
   }
-  if (data.quote_reserve > 79) {
-    GMGN_CHECK_RESULT.quote_reserve = true;
+  GMGN_CHECK_RESULT.quote_reserve.value = parseInt(data.quote_reserve);
+  if (data.quote_reserve > 100) {
+    GMGN_CHECK_RESULT.quote_reserve.res = true;
+  } else {
+    GMGN_CHECK_RESULT.quote_reserve.res = false;
   }
-  //   return res;
-  await chrome.storage.local.set({
-    gmgn_check_result: GMGN_CHECK_RESULT,
-  });
+  await updateStorage();
 };
 const realtimeTokenPrice = async function (data) {
   let usd_price = data.usd_price;
@@ -102,11 +153,6 @@ const realtimeTokenPrice = async function (data) {
 
 const topHoldersCheck = async function (data) {
   let top50 = data.slice(0, 50);
-  //   let res = {
-  //     profit_check: false,
-  //     bad_wallet_check: false,
-  //     flow_out_check: false,
-  //   };
   profit_num = 0;
   bad_wallet_num = 0;
   flow_out_num = 0;
@@ -122,19 +168,33 @@ const topHoldersCheck = async function (data) {
       flow_out_num += 1;
     }
   }
+  GMGN_CHECK_RESULT.profit_check.value = profit_num;
   if (profit_num > 25) {
-    GMGN_CHECK_RESULT.profit_check = true;
+    GMGN_CHECK_RESULT.profit_check.res = true;
+  } else {
+    GMGN_CHECK_RESULT.profit_check.res = false;
   }
+  GMGN_CHECK_RESULT.bad_wallet_check.value = bad_wallet_num;
   if (bad_wallet_num < 5) {
-    GMGN_CHECK_RESULT.bad_wallet_check = true;
+    GMGN_CHECK_RESULT.bad_wallet_check.res = true;
+  } else {
+    GMGN_CHECK_RESULT.bad_wallet_check.res = false;
   }
+  GMGN_CHECK_RESULT.flow_out_check.value = flow_out_num;
   if (flow_out_num < 5) {
-    GMGN_CHECK_RESULT.flow_out_check = true;
+    GMGN_CHECK_RESULT.flow_out_check.res = true;
+  } else {
+    GMGN_CHECK_RESULT.flow_out_check.res = false;
   }
-  //   return res;
-  await chrome.storage.local.set({
-    gmgn_check_result: GMGN_CHECK_RESULT,
-  });
+  await updateStorage();
+};
+const sendMessage = function (type) {
+  chrome.runtime.sendMessage(
+    { type: type, id: CHROME_CACHE_ID, cache_name: CHROME_CACHE_NAME },
+    function (response) {
+      //   console.log("收到来自后台的回复：" + response);
+    }
+  );
 };
 
 window.addEventListener("message", async (e) => {
@@ -147,12 +207,13 @@ window.addEventListener("message", async (e) => {
       await securityCheck(response.data);
       //   console.log("rrrrrrrr", res);
       console.log("cccccccc", GMGN_CHECK_RESULT);
-      chrome.runtime.sendMessage(
-        { type: "token_security_sol" },
-        function (response) {
-          //   console.log("收到来自后台的回复：" + response);
-        }
-      );
+      //   chrome.runtime.sendMessage(
+      //     { type: "token_security_sol", cache_name: CHROME_CACHE_NAME },
+      //     function (response) {
+      //       //   console.log("收到来自后台的回复：" + response);
+      //     }
+      //   );
+      sendMessage("token_security_sol");
     }
     //狙击者数据接口
     // if (
@@ -168,9 +229,13 @@ window.addEventListener("message", async (e) => {
       await tokenInfoCheck(response.data);
       //   console.log("rrrrrrrr", res);
       console.log("cccccccc", GMGN_CHECK_RESULT);
-      chrome.runtime.sendMessage({ type: "token_info" }, function (response) {
-        //   console.log("收到来自后台的回复：" + response);
-      });
+      //   chrome.runtime.sendMessage(
+      //     { type: "token_info", cache_name: CHROME_CACHE_NAME },
+      //     function (response) {
+      //       //   console.log("收到来自后台的回复：" + response);
+      //     }
+      //   );
+      sendMessage("token_info");
     }
     //token pool info 接口
     if (url.startsWith("https://gmgn.ai/api/v1/token_pool_info_sol/sol")) {
@@ -178,12 +243,13 @@ window.addEventListener("message", async (e) => {
       await tokenPoolInfoCheck(response.data);
       //   console.log("rrrrrrrr", res);
       console.log("cccccccc", GMGN_CHECK_RESULT);
-      chrome.runtime.sendMessage(
-        { type: "token_pool_info_sol" },
-        function (response) {
-          //   console.log("收到来自后台的回复：" + response);
-        }
-      );
+      //   chrome.runtime.sendMessage(
+      //     { type: "token_pool_info_sol", cache_name: CHROME_CACHE_NAME },
+      //     function (response) {
+      //       //   console.log("收到来自后台的回复：" + response);
+      //     }
+      //   );
+      sendMessage("token_pool_info_sol");
     }
     //实时价格接口
     if (
@@ -194,12 +260,13 @@ window.addEventListener("message", async (e) => {
       let response = JSON.parse(e.data.text);
       await realtimeTokenPrice(response.data);
       console.log("cccccccc", GMGN_CHECK_RESULT);
-      chrome.runtime.sendMessage(
-        { type: "realtime_token_price" },
-        function (response) {
-          //   console.log("收到来自后台的回复：" + response);
-        }
-      );
+      //   chrome.runtime.sendMessage(
+      //     { type: "realtime_token_price", cache_name: CHROME_CACHE_NAME },
+      //     function (response) {
+      //       //   console.log("收到来自后台的回复：" + response);
+      //     }
+      //   );
+      sendMessage("realtime_token_price");
     }
     //持有者排名接口
     if (
@@ -211,9 +278,13 @@ window.addEventListener("message", async (e) => {
       await topHoldersCheck(response.data);
       //   console.log("rrrrrrrr", res);
       console.log("cccccccc", GMGN_CHECK_RESULT);
-      chrome.runtime.sendMessage({ type: "top_holders" }, function (response) {
-        //   console.log("收到来自后台的回复：" + response);
-      });
+      //   chrome.runtime.sendMessage(
+      //     { type: "top_holders", cache_name: CHROME_CACHE_NAME },
+      //     function (response) {
+      //       //   console.log("收到来自后台的回复：" + response);
+      //     }
+      //   );
+      sendMessage("top_holders");
     }
   }
 });
--- popup.js
+++ popup.js
@@ -1,67 +1,142 @@
-// document.getElementById("data_check").addEventListener("click", async () => {
-//   chrome.runtime.sendMessage({ greeting: "data_check" }, function (response) {
-//     alert("数据采集成功");
-//   });
-// });
-
-const updateResult = async function () {
+const updateResult = async function (cache_name, type = "all") {
+  // if (CHROME_CACHE_NAME == null) {
+  //   CHROME_CACHE_NAME = getCacheName();
+  // }
+  console.log("hhhhyhh", cache_name);
   let gmgn_check_result = {};
   let res = await chrome.storage.local.get();
   console.log("local", res);
-  if (res["gmgn_check_result"]) {
-    gmgn_check_result = {
-      ...gmgn_check_result,
-      ...res["gmgn_check_result"],
-    };
+  if (res[cache_name]) {
+    gmgn_check_result = res[cache_name];
+  } else {
+    console.log("local storage is empty");
+    return;
   }
-  if (typeof gmgn_check_result.is_burn != "undefined") {
-    let text = gmgn_check_result.is_burn ? "通过" : "未通过";
-    document.querySelector("#is_burn > span").innerHTML = text;
+  if (type == "token_security_sol" || type == "all") {
+    if (typeof gmgn_check_result.is_burn != "undefined") {
+      let class_name = gmgn_check_result.is_burn.res ? "success" : "fail";
+      document.querySelector("#is_burn").className = class_name;
+      if (gmgn_check_result.is_burn.value != null) {
+        document.querySelector("#is_burn .data").textContent =
+          gmgn_check_result.is_burn.value;
+      }
+    }
+    if (typeof gmgn_check_result.not_black_list != "undefined") {
+      let class_name = gmgn_check_result.not_black_list.res
+        ? "success"
+        : "fail";
+      document.querySelector("#not_black_list").className = class_name;
+      if (gmgn_check_result.not_black_list.value != null) {
+        document.querySelector("#not_black_list .data").textContent =
+          gmgn_check_result.not_black_list.value;
+      }
+    }
+    if (typeof gmgn_check_result.renounced_mint != "undefined") {
+      let class_name = gmgn_check_result.renounced_mint.res
+        ? "success"
+        : "fail";
+      document.querySelector("#renounced_mint").className = class_name;
+      if (gmgn_check_result.renounced_mint.value != null) {
+        document.querySelector("#renounced_mint .data").textContent =
+          gmgn_check_result.renounced_mint.value;
+      }
+    }
+    if (typeof gmgn_check_result.top_10_holder != "undefined") {
+      let class_name = gmgn_check_result.top_10_holder.res ? "success" : "fail";
+      document.querySelector("#top_10_holder").className = class_name;
+      if (gmgn_check_result.top_10_holder.value != null) {
+        document.querySelector("#top_10_holder .data").textContent =
+          gmgn_check_result.top_10_holder.value;
+      }
+    }
   }
-  if (typeof gmgn_check_result.not_black_list != "undefined") {
-    let text = gmgn_check_result.not_black_list ? "通过" : "未通过";
-    document.querySelector("#not_black_list > span").innerHTML = text;
+  if (type == "token_info" || type == "all") {
+    if (typeof gmgn_check_result.holder_count != "undefined") {
+      let class_name = gmgn_check_result.holder_count.res ? "success" : "fail";
+      document.querySelector("#holder_count").className = class_name;
+      if (gmgn_check_result.holder_count.value != null) {
+        document.querySelector("#holder_count .data").textContent =
+          gmgn_check_result.holder_count.value;
+      }
+    }
   }
-  if (typeof gmgn_check_result.renounced_mint != "undefined") {
-    let text = gmgn_check_result.renounced_mint ? "通过" : "未通过";
-    document.querySelector("#renounced_mint > span").innerHTML = text;
+  if (type == "token_pool_info_sol" || type == "all") {
+    if (typeof gmgn_check_result.initial_quote_reserve != "undefined") {
+      let class_name = gmgn_check_result.initial_quote_reserve.res
+        ? "success"
+        : "fail";
+      document.querySelector("#initial_quote_reserve").className = class_name;
+      if (gmgn_check_result.initial_quote_reserve.value != null) {
+        document.querySelector("#initial_quote_reserve .data").textContent =
+          gmgn_check_result.initial_quote_reserve.value;
+      }
+    }
+    if (typeof gmgn_check_result.quote_reserve != "undefined") {
+      let class_name = gmgn_check_result.quote_reserve.res ? "success" : "fail";
+      document.querySelector("#quote_reserve").className = class_name;
+      if (gmgn_check_result.quote_reserve.value != null) {
+        document.querySelector("#quote_reserve .data").textContent =
+          gmgn_check_result.quote_reserve.value;
+      }
+    }
   }
-  if (typeof gmgn_check_result.top_10_holder != "undefined") {
-    let text = gmgn_check_result.top_10_holder ? "通过" : "未通过";
-    document.querySelector("#top_10_holder > span").innerHTML = text;
+  if (type == "realtime_token_price" || type == "all") {
+    if (typeof gmgn_check_result.market_value != "undefined") {
+      let class_name = gmgn_check_result.market_value.res ? "success" : "fail";
+      document.querySelector("#market_value").className = class_name;
+      if (gmgn_check_result.market_value.value != null) {
+        document.querySelector("#market_value .data").textContent =
+          gmgn_check_result.market_value.value;
+      }
+    }
   }
-  if (typeof gmgn_check_result.holder_count != "undefined") {
-    let text = gmgn_check_result.holder_count ? "通过" : "未通过";
-    document.querySelector("#holder_count > span").innerHTML = text;
-  }
-  if (typeof gmgn_check_result.initial_quote_reserve != "undefined") {
-    let text = gmgn_check_result.initial_quote_reserve ? "通过" : "未通过";
-    document.querySelector("#initial_quote_reserve > span").innerHTML = text;
-  }
-  if (typeof gmgn_check_result.quote_reserve != "undefined") {
-    let text = gmgn_check_result.quote_reserve ? "通过" : "未通过";
-    document.querySelector("#quote_reserve > span").innerHTML = text;
-  }
-  if (typeof gmgn_check_result.market_value != "undefined") {
-    let text = gmgn_check_result.market_value ? "通过" : "未通过";
-    document.querySelector("#market_value > span").innerHTML = text;
-  }
-  if (typeof gmgn_check_result.profit_check != "undefined") {
-    let text = gmgn_check_result.profit_check ? "通过" : "未通过";
-    document.querySelector("#profit_check > span").innerHTML = text;
-  }
-  if (typeof gmgn_check_result.bad_wallet_check != "undefined") {
-    let text = gmgn_check_result.bad_wallet_check ? "通过" : "未通过";
-    document.querySelector("#bad_wallet_check > span").innerHTML = text;
-  }
-  if (typeof gmgn_check_result.flow_out_check != "undefined") {
-    let text = gmgn_check_result.flow_out_check ? "通过" : "未通过";
-    document.querySelector("#flow_out_check > span").innerHTML = text;
+  if (type == "top_holders" || type == "all") {
+    if (typeof gmgn_check_result.profit_check != "undefined") {
+      let class_name = gmgn_check_result.profit_check.res ? "success" : "fail";
+      document.querySelector("#profit_check").className = class_name;
+      if (gmgn_check_result.profit_check.value != null) {
+        document.querySelector("#profit_check .data").textContent =
+          gmgn_check_result.profit_check.value;
+      }
+    }
+    if (typeof gmgn_check_result.bad_wallet_check != "undefined") {
+      let class_name = gmgn_check_result.bad_wallet_check.res
+        ? "success"
+        : "fail";
+      document.querySelector("#bad_wallet_check").className = class_name;
+      if (gmgn_check_result.bad_wallet_check.value != null) {
+        document.querySelector("#bad_wallet_check .data").textContent =
+          gmgn_check_result.bad_wallet_check.value;
+      }
+    }
+    if (typeof gmgn_check_result.flow_out_check != "undefined") {
+      let class_name = gmgn_check_result.flow_out_check.res
+        ? "success"
+        : "fail";
+      document.querySelector("#flow_out_check").className = class_name;
+      if (gmgn_check_result.flow_out_check.value != null) {
+        document.querySelector("#flow_out_check .data").textContent =
+          gmgn_check_result.flow_out_check.value;
+      }
+    }
   }
 };
 
 document.addEventListener("DOMContentLoaded", async function () {
-  updateResult();
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      let currentTab = tabs[0];
+      let id = currentTab.url.split("/").pop();
+      let cache_name = `gmgn_check_${id}`;
+      document.querySelector("#gmgn_id").textContent = id;
+      console.log("init cache_name", cache_name);
+      updateResult(cache_name);
+    }
+  );
 });
 
 // 监听来自content-script的消息
@@ -71,70 +146,12 @@ chrome.runtime.onMessage.addListener(async function (
   sendResponse
 ) {
   console.log("收到来自content-script的消息：", request);
-  let gmgn_check_result = {};
-  let res = await chrome.storage.local.get();
-  console.log("local", res);
-  if (res["gmgn_check_result"]) {
-    gmgn_check_result = {
-      ...gmgn_check_result,
-      ...res["gmgn_check_result"],
-    };
-  }
-  // let data = request.data;
-  if (request.type == "token_security_sol") {
-    if (typeof gmgn_check_result.is_burn != "undefined") {
-      let text = gmgn_check_result.is_burn ? "通过" : "未通过";
-      document.querySelector("#is_burn > span").innerHTML = text;
-    }
-    if (typeof gmgn_check_result.not_black_list != "undefined") {
-      let text = gmgn_check_result.not_black_list ? "通过" : "未通过";
-      document.querySelector("#not_black_list > span").innerHTML = text;
-    }
-    if (typeof gmgn_check_result.renounced_mint != "undefined") {
-      let text = gmgn_check_result.renounced_mint ? "通过" : "未通过";
-      document.querySelector("#renounced_mint > span").innerHTML = text;
-    }
-    if (typeof gmgn_check_result.top_10_holder != "undefined") {
-      let text = gmgn_check_result.top_10_holder ? "通过" : "未通过";
-      document.querySelector("#top_10_holder > span").innerHTML = text;
-    }
-  }
-  if (request.type == "token_info") {
-    if (typeof gmgn_check_result.holder_count != "undefined") {
-      let text = gmgn_check_result.holder_count ? "通过" : "未通过";
-      document.querySelector("#holder_count > span").innerHTML = text;
-    }
-  }
-  if (request.type == "token_pool_info_sol") {
-    if (typeof gmgn_check_result.initial_quote_reserve != "undefined") {
-      let text = gmgn_check_result.initial_quote_reserve ? "通过" : "未通过";
-      document.querySelector("#initial_quote_reserve > span").innerHTML = text;
-    }
-    if (typeof gmgn_check_result.quote_reserve != "undefined") {
-      let text = gmgn_check_result.quote_reserve ? "通过" : "未通过";
-      document.querySelector("#quote_reserve > span").innerHTML = text;
-    }
-  }
-  if (request.type == "realtime_token_price") {
-    if (typeof gmgn_check_result.market_value != "undefined") {
-      let text = gmgn_check_result.market_value ? "通过" : "未通过";
-      document.querySelector("#market_value > span").innerHTML = text;
-    }
-  }
-  if (request.type == "top_holders") {
-    if (typeof gmgn_check_result.profit_check != "undefined") {
-      let text = gmgn_check_result.profit_check ? "通过" : "未通过";
-      document.querySelector("#profit_check > span").innerHTML = text;
-    }
-    if (typeof gmgn_check_result.bad_wallet_check != "undefined") {
-      let text = gmgn_check_result.bad_wallet_check ? "通过" : "未通过";
-      document.querySelector("#bad_wallet_check > span").innerHTML = text;
-    }
-    if (typeof gmgn_check_result.flow_out_check != "undefined") {
-      let text = gmgn_check_result.flow_out_check ? "通过" : "未通过";
-      document.querySelector("#flow_out_check > span").innerHTML = text;
-    }
-  }
+  let type = request.type;
+  let cache_name = request.cache_name;
+  let id = request.id;
+  document.querySelector("#gmgn_id").textContent = id;
+  updateResult(cache_name, type);
+
   // console.log(request, sender, sendResponse);
   // sendResponse("我是后台，我已收到你的消息：" + JSON.stringify(request));
 });
